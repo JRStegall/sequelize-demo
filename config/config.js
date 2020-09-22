60ba97ac9901ddd2af5ec9eb5845e5be97a6b68c
Jeremy <jeremy.stegall77@gmail.com>
2020-09-21 21:48:13 -0400

    Temp fix for Heroku connection locally

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "postgres",
+    "dialect": "postgres"
     //For Heroku, uncomment this:
     //dialectOptions: {
     //  ssl: {
