87c6999a0b623d04fb2e9d58ca816dd9678e9eb0
Jeremy <jeremy.stegall77@gmail.com>
2020-09-21 21:19:37 -0400

    Add encryption to password. Add middleware and sign-up route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const pbkdf2 = require('pbkdf2'); //encryption
+const models = require('./models');
 
 require('dotenv').config();//initialize dotenv to use environment variables
 
@@ -16,10 +17,34 @@ app.use(session({
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//encryption middleware function
+function encryptPassword(req, res, next){
+    if(req.body.password){
+    var key = pbkdf2.pbkdf2Sync(
+        req.body.password, process.env.PASSWORD_SALT, 3600, 256, 'sha256'
+    );
+
+    var hash = key.toString('hex');
+    req.body.password = hash;
+    next();
+    }else{
+        res.send('Please add password');
+    }
+}
+
+
 app.get('/', function(req, res){
     res.send(process.env.HELLO_MESSAGE);
-})
+});
+
+app.post('/sign-up', encryptPassword, function(req, res){
+    models.user.create({ username: req.body.username, password: req.body.password})
+    .then(function (user){
+        console.log('new user created: ', user);
+        res.send(user);
+    })
+});
 
 app.listen(process.env.PORT, function(){
     console.log(`My API is listening to port ${process.env.PORT}`)
-})
+});
