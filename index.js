8def18e3fd4ce8efd297abac72db8b6166251204
Jeremy <jeremy.stegall77@gmail.com>
2020-09-21 20:41:13 -0400

    Start express file and test it works with simple route

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const pbkdf2 = require('pbkdf2'); //encryption
+
+require('dotenv').config();//initialize dotenv to use environment variables
+
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 }
+}));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/', function(req, res){
+    res.send("Sup fuckers!");
+})
+
+app.listen(process.env.PORT, function(){
+    console.log(`My API is listening to port ${process.env.PORT}`)
+})
